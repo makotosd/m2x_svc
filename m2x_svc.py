542074f176a1b7e1616a7c443a6cb2fd6d63bb8b
mm <mmm@zzz.com>
2019-12-04 23:51:17 +0900

    Good.

--- m2x_svc.py
+++ m2x_svc.py
@@ -1,50 +1,29 @@
 # -*- coding: utf-8 -*-
 from m2x.client import M2XClient
-import json
-from flask import Flask, jsonify
-import datetime
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    (t, h) = get_temperature()
-    #(t, h) = get_temperature_dummy()
-    dic = {
-        'temperature': t,
-        'humidity': h
-    }
-    dic2 = {
-        'timestamp': datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"),
-        'value': dic
-    }
-    return jsonify(dic2)
+@app.route('/m2x', methods=['POST'])
+def post():
+    posted_json = request.get_json()
+    temperature = posted_json['value']['temperature']
+    humidity = posted_json['value']['humidity']
 
-
-
-def get_from_m2x():
     client = M2XClient(key='81faa53c80c0c084e797d706bc84be25')  # API-KEY
-
     device = client.device('7870230c081b7f4f678dde08bc7bcba7')  # DEVICE-ID
-    #  client.devices()
-    device.stream("temperature").values(limit=6)
+    stream_temperature = device.stream('temperature')
+    stream_humidity = device.stream('humidity')
 
-    # Get raw HTTP response
-    raw = client.last_response.raw
+    response_temperature = stream_temperature.add_value(float(temperature))
+    response_humidity = stream_humidity.add_value(float(humidity))
 
-    # Get HTTP respose status code (e.g. `200`)
-    status = client.last_response.status
+    response = {'temperature': response_temperature,
+                'humidity': response_humidity}
 
-    # Get HTTP response headers
-    headers = client.last_response.headers
-
-    # Get json data returned in HTTP response
-    res_json = client.last_response.json
-    values = res_json['values']
-
-    return values
+    return jsonify(response)
 
 
 if __name__ == '__main__':
