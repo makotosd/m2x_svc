540fb9caf6b572d70bd95494affb857cf2ed0583
makotosd <mouri.makoto@gmail.com>
2020-04-18 22:32:43 +0900

    open 8081

--- Run.sh
+++ Run.sh
@@ -1,1 +1,5 @@
-docker run -d --rm --name m2x --network sensor-network tmc05/m2x_svc
+#!/bin/sh
+#
+#
+#docker run -d --rm --name m2x --network sensor-network tmc05/m2x_svc
+docker run -d --rm --name m2x --network sensor-network -p 8081:8080 tmc05/m2x_svc
